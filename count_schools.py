2f7ae5d442590ff73d7dab7ed66d3a617220e06a
Sebastian Lopez <seb.lopez331@gmail.com>
2024-08-18 15:11:59 -0400

    feat: show diff if colummns are not as expected

--- count_schools.py
+++ count_schools.py
@@ -95,7 +95,7 @@ Finally, here is a breakdown for STATUS05
 
 def load_csv(
     filename: str, encoding: str = 'Windows-1252'
-) -> tuple[list[dict[str, any]], set[str]]:
+) -> tuple[list[dict[str, any]], list[str]]:
     """This function loads data from an inputted CSV file with the specified encoding, defaulted to Windows-1252.
 
     If the file does not exist or if there are any errors associated with loading the data, we exit from the script 
@@ -127,8 +127,8 @@ def load_csv(
     
     Returns
     -------
-    (list[dict[str, any]], set[str])
-        A tuple that contains a list of dicts that represent each row, and a set with strings that represent the names
+    (list[dict[str, any]], list[str])
+        A tuple that contains a list of dicts that represent each row, and a list with strings that represent the names
         of each column.
     """
     loaded_data = []
@@ -175,11 +175,11 @@ def load_csv(
         print(f'Error loading file: {e}')
         exit(1)
     
-    return loaded_data, set(column_names)
+    return loaded_data, column_names
 
 
 def count_schools(
-    data: list[dict[str, any]], column_names: set[str], school_name_column: str
+    data: list[dict[str, any]], column_names: list[str], school_name_column: str
 ) -> int:
     """This function will count the total schools in the provided loaded data.
 
@@ -206,12 +206,18 @@ def count_schools(
         exit(1)
 
     distinct_schools = set()
+    column_set = set(column_names)
 
     for line, entry in enumerate(data):
         # Check here if all the required keys in the schema are present
-        if not column_names <= entry.keys():
-            missing_keys = column_names - entry.keys()
-            print(f'Error counting schools: Entry {entry} in line {line} is missing the required keys: {missing_keys}.')
+        if column_set != entry.keys():
+            key_list = list(entry.keys())
+            diff = list(entry.keys() ^ column_set)
+            print(
+                f'Error counting schools: Entry #{line} has keys {key_list} when the expected keys are '
+                f'{column_names}.\n'
+                f'Here is a diff for ease of reference: {diff}.'
+            )
             exit(1)
         
         school = entry[school_name_column]
@@ -229,5 +235,5 @@ def count_schools_for_each_state(
     """
 
 loaded_data, schema = load_csv("school_data.csv")
-num_schools = count_schools(loaded_data, schema)
+num_schools = count_schools(loaded_data, schema, 'SCHNAM05')
 print(num_schools)
