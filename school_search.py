1c9525cfce55c17561f0ed256754c1a5c6a43247
Sebastian Lopez <seb.lopez331@gmail.com>
2024-08-19 19:22:54 -0400

    feat: added a school search script

--- school_search.py
+++ school_search.py
@@ -3,10 +3,19 @@ import csv
 """
 We can use a predefined list of states, cities, and schools
 """
+# Constants
+SCHOOL_NAME_COLUMN = 'SCHNAM05'
+CITY_COLUMN = 'LCITY05'
+STATE_COLUMN = 'LSTATE05'
 
-def load_csv(
-    filename: str, encoding: str = 'Windows-1252'
-) -> tuple[list[dict[str, any]], list[str]]:
+# We use these as stop words because we are looking for schools anyways
+STOP_WORDS = set([
+    'school', 
+    'academy', 
+    'institute'
+])
+
+def load_csv(filename: str, encoding: str = 'Windows-1252') -> tuple[list[dict[str, any]], list[str]]:
     """This function loads data from an inputted CSV file with the specified encoding, defaulted to Windows-1252.
 
     If the file does not exist or if there are any errors associated with loading the data, we exit from the script 
@@ -114,12 +123,97 @@ def find_distinct(data: dict[str, any], column_names: list[str], column: str) ->
 
     return distinct
 
+
+def tokenize(text: str) -> list[str]:
+    # We want to replace all punctuation characters with space
+    punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    text = text.lower()
+    text = ''.join(' ' if char in punctuation else char for char in text)
+    text = ''.join(char for char in text if char.isalnum() or char.isspace())
+    tokens = text.split()
+    filtered = filter(lambda word: word not in STOP_WORDS, tokens)
+    return list(filtered)
+
+
+def compute_exact_match(entry: dict[str, any], keywords: list[str]) -> float:
+    school_name_tokens = tokenize(entry[SCHOOL_NAME_COLUMN])
+    city_tokens = tokenize(entry[CITY_COLUMN])
+    state_token = entry[STATE_COLUMN].lower()
+
+    keywords_set = set(keywords)
+    
+    return 1.0 if (keywords_set.issubset(school_name_tokens) or
+                 keywords_set.issubset(city_tokens) or
+                 keywords_set.issubset([state_token])) else 0.0
+
+
+
+def compute_partial_match(entry: dict[str, any], keywords: list[str]) -> float:
+    school_name_tokens = tokenize(entry[SCHOOL_NAME_COLUMN])
+    city_tokens = tokenize(entry[CITY_COLUMN])
+    state_token = entry[STATE_COLUMN].lower()
+
+    all_tokens = set(school_name_tokens + city_tokens + [state_token])
+
+    total_matches = 0
+    for word in keywords:
+        if word in all_tokens:
+            total_matches += 1
+    
+    return float(total_matches) / len(keywords)
+
+
+def compute_city_match(entry: dict[str, any], keywords: list[str]) -> float:
+    city_tokens = set(tokenize(entry[CITY_COLUMN]))
+
+    total_matches = 0
+    for word in keywords:
+        if word in city_tokens:
+            total_matches += 1
+    
+    return float(total_matches) / len(keywords)
+
+
+def compute_rank(entry: dict[str, any], keywords: list[str]) -> float:
+    exact_match = compute_exact_match(entry, keywords)
+    partial_match = compute_partial_match(entry, keywords)
+    location_score = compute_city_match(entry, keywords)
+    final_score = 0.5*exact_match + 0.3*partial_match + 0.05*location_score
+    return final_score
+    
+
 loaded_data, column_names = load_csv("school_data.csv")
-SCHOOL_NAME_COLUMN = 'SCHNAM05'
-CITY_COLUMN = 'LCITY05'
-STATE_COLUMN = 'LSTATE05'
+print()
 
 
-distinct_schools = find_distinct(loaded_data, column_names, column=)
-distinct_cities = find_distinct(loaded_data, column_names, column=)
+def search_schools(query: str, n: int = 3) -> list[dict[str, any]]:
+    keywords = tokenize(query)
+    top_results = []
 
+    for entry in loaded_data:
+        score = compute_rank(entry, keywords)
+        
+        if len(top_results) < n:
+            top_results.append({'entry': entry, 'score': score})
+            continue
+
+        # This may be optimized for a heap if we choose n entries, but with 3 entries it is faster as is.
+        min_index = 0
+
+        for i in range(1, n): # This is O(1) for small n, like n = 3.
+            if top_results[i]['score'] <= top_results[min_index]['score']:
+                min_index = i
+
+        if score >= top_results[min_index]['score']:
+            top_results.pop(min_index)
+            top_results.append({'entry': entry, 'score': score})
+    
+    top_results.sort(key=lambda result: result['score'])
+
+    print('Search results for query: ', query)
+    for result in top_results[::-1]:
+        entry = result['entry']
+        school = entry[SCHOOL_NAME_COLUMN]
+        city = entry[CITY_COLUMN]
+        state = entry[STATE_COLUMN]
+        print(f'{school}, {city}, {state}, {result["score"]}')
